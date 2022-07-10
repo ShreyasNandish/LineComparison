1ea68a6fd3a1a63714a269dca3aab1032c7a5159
ShreyasNandish <shreyasnandish4@gmail.com>
2022-07-10 19:26:30 +0530

    Modified:Added the program to calculate length between 2 lines

--- src/com/bridgelabz/LineComparsion/LineComparsion.java
+++ src/com/bridgelabz/LineComparsion/LineComparsion.java
@@ -3,8 +3,24 @@ package com.bridgelabz.LineComparsion;
 import java.util.Scanner;
 
 public class LineComparsion {
+        public static void calculatelength(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter x1 : ");
+        int x1 = sc.nextInt();
+        System.out.println("Enter y1 : ");
+        int y1 = sc.nextInt();
+        System.out.println("Enter x2 : ");
+        int x2 = sc.nextInt();
+        System.out.println("Enter y2 : ");
+        int y2 = sc.nextInt();
+
+        double a=(float)Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2)*1.0);
+        System.out.println("Length of line is :"+a);
+        }
+
     public static void main(String[] args) {
         System.out.println("Welcome to Line Comparison program ");
+        calculatelength();
 
     }
 }
