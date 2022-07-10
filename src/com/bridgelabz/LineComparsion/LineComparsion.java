535c6017532d8773c9ded9856381618ffa9922b1
ShreyasNandish <shreyasnandish4@gmail.com>
2022-07-10 19:03:04 +0530

    Added Line Comparsion program

---
+++ src/com/bridgelabz/LineComparsion/LineComparsion.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.LineComparsion;
+
+import java.util.Scanner;
+
+public class LineComparsion {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line Comparison program ");
+
+    }
+}
